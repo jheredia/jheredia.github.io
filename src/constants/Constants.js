33e7695443a472b80c114ff2a6614aa4831daaab
Joaquin Heredia <joaquinheredia93@gmail.com>
2019-06-14 22:13:39 -0400

    Constants definitions file

---
+++ src/constants/Constants.js
@@ -0,0 +1,9 @@
+// Default avatar URL
+export const AvatarUrl =
+  "https://avatars1.githubusercontent.com/u/3941955?s=460&v=4";
+
+// Avatar available frame types
+export const CircleFrame = "circle";
+export const RoundedFrame = "rounded";
+export const SquareFrame = "square";
+export const ThumbnailFrame = "img-thumbnail";
